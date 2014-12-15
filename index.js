de90ee7dbe13bcd6168aec9e04c8d74bfa948dfc
Scott Ivey <scott.ivey@gmail.com>
2014-12-14 21:08:22 -0500

    add default filter for tasks starting with leading underscore, and for the _bash-completion-helper task itself.

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ var getTasks = function(gulpRef) {
     return _.keys(gulpRef.tasks);
 };
 
+var defaultFilter = function(taskName) {
+    if (taskName.indexOf('_') === 0) {
+        return false;
+    }
+    return true;
+};
+
 module.exports = function(gulp, options) {
     options || (options = {});
-    gulp.task('_bash-completion-helper', function() {
+    var filter = options.filter || defaultFilter;
+    var COMPLETION_TASK = '_bash-completion-helper';
+    gulp.task(COMPLETION_TASK, function() {
         var tasks = getTasks(gulp);
-        if (options.filter) {
-            tasks = _.filter(tasks, options.filter);
-        }
+        tasks = _.filter(tasks, function(task) {
+            if (task === COMPLETION_TASK) {
+                return false;
+            }
+            return filter(task);
+        });
         console.log(tasks.join('\n'));
     });
 };
